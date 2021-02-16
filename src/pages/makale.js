1e948ce57222de4d20e4abc0e2bb4bcb7b26537f
burrrrakozenc <brk.ozenc@gmail.com>
2021-02-17 01:24:13 +0300

    article pages updated

--- src/pages/makale.js
+++ src/pages/makale.js
@@ -1,67 +1,71 @@
-// import React from "react";
-// // import { Link } from 'gatsby'
-// import Layout from '../components/layout'
-// import { graphql, Link } from 'gatsby'
-// import '../styles/articles.scss'
+import React from "react";
+// import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import { graphql, Link } from 'gatsby'
+import '../styles/articles.scss'
 
-// const Article = ({ data }) => {
+const Article = ({ data }) => {
 
-//     const { articles: { nodes: article },
-//     } = data
-//     return (
-//         <Layout>
-//             <div className="articles-wrapper">
-//                 <div className="articles-container">
-//                     {article.map((articl) => {
-//                         return (
-//                             <Link to={`../${articl.slug}`}>
-//                                 <div className="card-wrapper">
-//                                     <div className="blog-card">
+    const { articles: { nodes: article },
+    } = data
+    return (
+        <Layout>
+            <div className="articles-wrapper">
+                <div className="articles-container">
+                    {article.map((articl) => {
+                        return (
+                            <Link to={`../${articl.slug}`}>
+                                <div className="card-wrapper">
+                                    <div className="blog-card">
 
-//                                         <img className="blog-img" alt="img" src={articl.image.childImageSharp.fluid.src} />
-//                                         <div className="text-overlay">
-//                                             <h2>{articl.title}</h2>
-//                                             <p>{articl.description}
-//                                             </p>
-//                                         </div>
-//                                     </div>
-//                                     <div className="credit">Created by <a className="creator-link" href="https://brettstevenson.io">Brett Stevenson</a></div>
-//                                 </div>
-//                             </Link>
-//                         )
-//                     })}
-//                 </div>
-//             </div>
-//         </Layout>
-//     )
+                                        <img className="blog-img" alt="img" src={articl.image.localFile.childImageSharp.fluid.src} />
+                                        <div className="text-overlay">
+                                            <h2>{articl.title}</h2>
+                                            <p>{articl.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="credit">Created by <a className="creator-link" href="https://brettstevenson.io">Brett Stevenson</a></div>
+                                </div>
+                            </Link>
+                        )
+                    })}
+                </div>
+            </div>
+        </Layout>
+    )
 
-// }
+}
 
-// export default Article
-// // (filter: {category: {name: {eq: "makale"}}}) 
-// export const pageQuery = graphql`
-//     {
-//         articles: allStrapiArticle{
-//             nodes {
-//               title
-//               author {
-//                 name
-//                 id
-//               }
-//               slug
-//               description
-//               id
-//               slug
-//             publishedAt
-//             image {
-//                 childImageSharp {
-//                 fluid {
-//                     src
-//                 }
-//                 }
-//                 id
-//             }
-//             }
-//           }
-//     }
-//   `
+export default Article
+// (filter: {category: {name: {eq: "makale"}}}) 
+export const pageQuery = graphql`
+    {
+        articles: allStrapiArticle{
+            nodes {
+              title
+              author {
+                name
+                id
+              }
+              slug
+              description
+              id
+              slug
+            publishedAt
+            image {
+                localFile {
+                  childImageSharp {
+                    fluid {
+                      src
+                    }
+                    id
+                  }
+                  id
+                }
+                id
+              }
+            }
+          }
+    }
+  `
--- src/pages/zizek.js
+++ src/pages/zizek.js
@@ -1,65 +1,69 @@
-// import React from "react";
-// import Layout from '../components/layout'
-// import { graphql, Link } from 'gatsby'
-// import '../styles/articles.scss'
+import React from "react";
+import Layout from '../components/layout'
+import { graphql, Link } from 'gatsby'
+import '../styles/articles.scss'
 
-// const Zizek = ({ data }) => {
+const Zizek = ({ data }) => {
 
-//     const { zizek: { nodes: zizek },
-//     } = data
-//     return (
-//         <Layout>
-//             <div className="articles-wrapper">
-//                 <div className="articles-container">
-//                     {zizek.map((ziz) => {
-//                         return (
-//                             <Link to={`../${ziz.slug}`}>
-//                                 <div className="card-wrapper">
-//                                     <div className="blog-card">
-//                                         <img className="blog-img" alt="img" src={ziz.image.childImageSharp.fluid.src} />
-//                                         <div className="text-overlay">
-//                                             <h2>{ziz.title}</h2>
-//                                             <p>{ziz.description}
-//                                             </p>
-//                                         </div>
-//                                     </div>
-//                                     <div className="credit">Created by <a className="creator-link" href="https://brettstevenson.io">Brett Stevenson</a></div>
-//                                 </div>
-//                             </Link>
-//                         )
-//                     })}
-//                 </div>
-//             </div>
-//         </Layout>
-//     )
+    const { zizek: { nodes: zizek },
+    } = data
+    return (
+        <Layout>
+            <div className="articles-wrapper">
+                <div className="articles-container">
+                    {zizek.map((ziz) => {
+                        return (
+                            <Link to={`../${ziz.slug}`}>
+                                <div className="card-wrapper">
+                                    <div className="blog-card">
+                                        <img className="blog-img" alt="img" src={ziz.image.localFile.childImageSharp.fluid.src} />
+                                        <div className="text-overlay">
+                                            <h2>{ziz.title}</h2>
+                                            <p>{ziz.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="credit">Created by <a className="creator-link" href="https://brettstevenson.io">Brett Stevenson</a></div>
+                                </div>
+                            </Link>
+                        )
+                    })}
+                </div>
+            </div>
+        </Layout>
+    )
 
-// }
+}
 
-// export default Zizek
-// // (filter: {category: {name: {eq: "zizek-calismalari"}}}) 
-// export const pageQuery = graphql`
-//     {
-//         zizek: allStrapiArticle{
-//             nodes {
-//               title
-//               author {
-//                 name
-//                 id
-//               }
-//               slug
-//               description
-//               id
-//               slug
-//             publishedAt
-//             image {
-//                 childImageSharp {
-//                 fluid {
-//                     src
-//                 }
-//                 }
-//                 id
-//             }
-//             }
-//           }
-//     }
-//   `
+export default Zizek
+// (filter: {category: {name: {eq: "zizek-calismalari"}}}) 
+export const pageQuery = graphql`
+    {
+        zizek: allStrapiArticle{
+            nodes {
+              title
+              author {
+                name
+                id
+              }
+              slug
+              description
+              id
+              slug
+            publishedAt
+            image {
+                localFile {
+                  childImageSharp {
+                    fluid {
+                      src
+                    }
+                    id
+                  }
+                  id
+                }
+                id
+              }
+            }
+          }
+    }
+  `
